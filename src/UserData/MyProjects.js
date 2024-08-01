7657b35bf9b8cd01e0a96c00f4ac288043902ee0
David Erenfeld <1020dudu@gmail.com>
2024-08-01 12:28:52 +0300

    Adding a new project

--- src/UserData/MyProjects.js
+++ src/UserData/MyProjects.js
@@ -3,7 +3,7 @@ export const myProjects = [
     projectName: "My Portfolio",
     githubLink: "https://github.com/DavidErenfeld/myPortfolio.git",
     websiteLink: "https://david-erenfeld.netlify.app/",
-    technologies: ["Javascript", "React", "HTML", "CSS"],
+    technologies: ["JavaScript", "React", "HTML", "CSS"],
   },
   {
     projectName: "Travel easily server",
@@ -11,7 +11,7 @@ export const myProjects = [
     websiteLink:
       "https://enigmatic-island-56921-258869278475.herokuapp.com/api-docs/",
     technologies: [
-      "Typescript",
+      "TypeScript",
       "NodeJS",
       "Express",
       "MongoDB",
@@ -20,15 +20,35 @@ export const myProjects = [
     ],
   },
   {
+    projectName: "Code Mentor server",
+    githubLink: "https://github.com/DavidErenfeld/Code_Mentor_Server.git",
+    websiteLink:
+      "https://boiling-springs-91338-006b899baec7.herokuapp.com/codeBlocks",
+    technologies: ["JavaScript", "NodeJS", "Express", "MongoDB", "Socket.IO"],
+  },
+  {
+    projectName: "Code Mentor front",
+    githubLink: "https://github.com/DavidErenfeld/Code_Mentor_Front.git",
+    websiteLink: "https://codementor-daviderenfeld.netlify.app/",
+    technologies: [
+      "JavaScript",
+      "React",
+      "CSS",
+      "HTML",
+      "MongoDB",
+      "Socket.IO",
+    ],
+  },
+  {
     projectName: "Travel easily front",
     githubLink: "https://github.com/DavidErenfeld/Travel-Easily-Front.git",
     websiteLink: "https://travel-easily.netlify.app/",
-    technologies: ["Typescript", "React", "HTML", "CSS"],
+    technologies: ["TypeScript", "React", "HTML", "CSS"],
   },
   {
     projectName: "Pig game",
     githubLink: "https://github.com/DavidErenfeld/Pig-Game.git",
     websiteLink: "https://tranquil-syrniki-b95ab5.netlify.app/",
-    technologies: ["HTML", "CSS", "Javascript"],
+    technologies: ["HTML", "CSS", "JavaScript"],
   },
 ];
